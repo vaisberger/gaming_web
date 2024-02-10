8c99e222702a397532ac58d52d1cf43ae6b4416f
vaisberger <yaelw123@gmail.com>
2024-02-11 00:36:28 +0200

    11.2-0

--- js-files/memo_game.js
+++ js-files/memo_game.js
@@ -1,1 +1,18 @@
 let cards[];
+fetch("/.vscode/cards.json")
+.then((res)=>res.json)
+.then((data)=>{
+   cards=[...data,...data]
+   mixcards();
+})
+function mixcards(){
+    let currentIndex=cards.length,randomIndex;
+    while (currentIndex > 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [cards[currentIndex], cards[randomIndex]] = [
+          cards[randomIndex], cards[currentIndex]];
+      }
+}
